4e51198cf1b466a24c0ec73aef4aac7df49d84c6
Abdelfattah <abdelfa2@vdi-scclab-047.lancs.local>
2023-10-22 14:45:29 +0100

    worked on the moon class and added it's animations

---
+++ Moon.java
@@ -0,0 +1,41 @@
+public class Moon{
+
+    //Variables that define moon parameters and measurements
+    SolarSystem solar; //implementing the solarsystem as a variable.
+    private double moonDistance;
+    private double moonDiameter;
+    private double moonAngle;
+    private double centreAngle;
+    private double centreRotation;
+    private String moonColor;
+
+    //Constructor
+    public Moon(SolarSystem solar, double moonDiameter, double moonAngle, double centreRotation,double centreAngle, double moonDistance, String moonColor){
+        this.solar = solar;
+        this.moonDistance = moonDistance;
+        this.moonDiameter = moonDiameter;
+        this.moonAngle = moonAngle;
+        this.moonColor = moonColor;
+        this.centreAngle = centreAngle;
+        this.centreRotation = centreRotation;
+    }
+
+    //Method to draw the moon with appropiate diameter, angle, distance and color
+    public void drawMoon(){
+        solar.drawSolarObjectAbout(moonDistance, moonAngle, moonDiameter, moonColor, centreRotation, centreAngle);
+    }
+
+    //Animating moon movement
+    public void moveMoon(){
+        moonAngle = moonAngle + 3;
+        centreAngle++;
+    }
+
+    //finished drawing method
+    public void finishedDrawing(){
+        solar.finishedDrawing();
+    }
+
+
+
+}
